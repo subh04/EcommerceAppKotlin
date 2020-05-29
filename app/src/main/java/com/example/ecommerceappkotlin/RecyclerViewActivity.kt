4547c9bada948ace0902f28a057da1577bac390b
Subhasish <subhasish469@gmail.com>
2020-05-29 21:41:41 +0530

    recycler View

---
+++ app/src/main/java/com/example/ecommerceappkotlin/RecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerceappkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+    }
+}
